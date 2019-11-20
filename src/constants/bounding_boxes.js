d37a09eeb75465891239d37b30d9d68f2cd547c2
Mikko Rauhala <mikko@rauhala.net>
2019-11-20 14:36:39 +0200

    Add Northern Europe

--- src/constants/bounding_boxes.js
+++ src/constants/bounding_boxes.js
@@ -2,6 +2,7 @@
 //          bottom left/upper right (y/x)
 const NL = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Netherlands', bbox: [314909.3659069278, 6470493.345653814, 859527.2396033217, 7176664.533565958] };
 const NL_NORTHSEA = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'NL + North Sea', bbox: [1960.19, 6551456.69, 846923.65, 7550440] };
+const NORTHERN_EUROPE = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Northern Europe', bbox: [9783.9814, 7073788.4324, 4045658.9844, 11574400.6058] };
 const WESTERN_EUROPE = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Western Europe', bbox: [-1180632.97, 4310394.19, 2049131.83, 9243688.99] };
 const EUROPE = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Europe', bbox: [-1490353.42, 4112245.37, 6853336.48, 12083665.89] };
 const BONAIRE = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Bonaire', bbox: [-7780684.59, 1234792.98, -7398676.19, 1536426.81] };
@@ -10,5 +11,5 @@ const N_AMERICA = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 
 const AFRICA = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Africa', bbox: [-2866702.01, -4305254.33, 5895029.92, 4750064.45] };
 const ASIA = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Asia', bbox: [2731570.69, 356319.28, 20019217.39, 13468334.39] };
 const AUSTRALIA = { projection: { code: 'EPSG:3857', name: 'Mercator' }, title: 'Australia', bbox: [10666107.54, -5730599.41, 17966528.52, 1237663.02] };
-export const BOUNDING_BOXES = [NL, NL_NORTHSEA, WESTERN_EUROPE, EUROPE,
+export const BOUNDING_BOXES = [NL, NL_NORTHSEA, NORTHERN_EUROPE, WESTERN_EUROPE, EUROPE,
   BONAIRE, SABA_EUST, N_AMERICA, AFRICA, ASIA, AUSTRALIA];
